b57136b385e449e76a8399a3a2426a88eedda0d9
Difan Zhang <alex@osqdu.org>
2016-03-07 02:00:19 -0800

    Bump version, uploaded to PyPi index.

--- dajaxice/__init__.py
+++ dajaxice/__init__.py
@@ -1,2 +1,2 @@
-__version__ = (0, 7, 0, 1)
+__version__ = (0, 7, 0, 2)
 __version__string__ = '.'.join(map(str, __version__))
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='django-dajaxice-ng',
     version=dajaxice.__version__string__,
     author='ifanr',
-    author_email='tifan@ifanr.com',
+    author_email='ifanrx@ifanr.com',
     description='Agnostic and easy to use ajax library for django',
     url='https://github.com/ifanrx/django-dajaxice',
     license='BSD',
@@ -18,7 +18,7 @@ setup(
                       "doesn't require any JS Framework."
                       "This is the next generation of dajaxice maintained by ifanrx."),
     install_requires=[
-        'Django>=1.5'
+        'Django>=1.7'
     ],
     classifiers=['Development Status :: 4 - Beta',
                 'Environment :: Web Environment',
