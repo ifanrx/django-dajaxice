dd1d3f78162bc772078f82b3377e95f126225b73
John Deng <denghuanzhong@gmail.com>
2016-03-11 11:11:12 +0800

    Django 1.9 compatibility

--- dajaxice/urls.py
+++ dajaxice/urls.py
@@ -1,11 +1,11 @@
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
 except ImportError:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import url
 
 from .views import DajaxiceRequest
 
-urlpatterns = patterns('dajaxice.views',
+urlpatterns = [
     url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
     url(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
-)
+]
